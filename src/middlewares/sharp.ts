7c4daee4cba8c0eb2aca40d68172867528b55f77
AYTU ULIŞ <aytugulis@gmail.com>
2022-08-21 12:00:40 +0300

    Solving image problem

--- src/middlewares/sharp.ts
+++ src/middlewares/sharp.ts
@@ -7,10 +7,27 @@ export const shapeImage = expressAsyncHandler(
     if (req.file) {
       const isUserImage = req.file.path.includes('user');
       sharp.cache(false);
+
+      if (!isUserImage) {
+        const buffer = await sharp(req.file.path)
+          .resize({ width: 1280, height: 720 })
+          .webp()
+          .toBuffer();
+
+        await sharp(buffer).toFile(
+          req.file.path.replace('.webp', '_banner.webp'),
+        );
+      }
+
       const buffer = await sharp(req.file.path)
-        .resize(isUserImage ? {} : { height: 900, width: 1600 })
-        .webp({ quality: 50 })
+        .resize(
+          isUserImage
+            ? { width: 100, height: 100, position: 'top' }
+            : { width: 288, height: 162 },
+        )
+        .webp()
         .toBuffer();
+
       await sharp(buffer).toFile(req.file.path);
     }
     next();
